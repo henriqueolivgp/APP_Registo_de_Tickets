462ad22ef52a119ab64a66a33d4c2d5947db7728
tiago rosado <tiagorosado@ipvc.pt>
2024-01-05 15:49:52 +0000

    navigation

--- app/src/main/java/com/example/app_registo_de_tickets/MainActivity.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.app_registo_de_tickets
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun navegarParaLogin(view: View) {
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
     }
 }
