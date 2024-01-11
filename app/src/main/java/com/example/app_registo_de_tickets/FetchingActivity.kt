cda82dc45e763b5b257f95344abc78e80e88e110
tiago rosado <tiagorosado@ipvc.pt>
2024-01-11 05:36:58 +0000

    get tickets

---
+++ app/src/main/java/com/example/app_registo_de_tickets/FetchingActivity.kt
@@ -0,0 +1,65 @@
+package com.example.app_registo_de_tickets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class FetchingActivity : AppCompatActivity() {
+    private lateinit var empRecyclerView: RecyclerView
+    private lateinit var tvLoadingData: TextView
+    private lateinit var empList: ArrayList<TicketsModel>
+    private lateinit var dbRef: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fetching)
+        empRecyclerView = findViewById(R.id.rvEmp)
+        empRecyclerView.layoutManager = LinearLayoutManager(this)
+        empRecyclerView.setHasFixedSize(true)
+        tvLoadingData = findViewById(R.id.tvLoadingData)
+
+        empList = arrayListOf<TicketsModel>()
+
+        getTicketsData()
+    }
+
+    private fun getTicketsData(){
+
+        empRecyclerView.visibility = View.GONE
+        tvLoadingData.visibility = View.VISIBLE
+
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Tickets")
+
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                empList.clear()
+                if (snapshot.exists()){
+                    for (empSnap in snapshot.children){
+                        val ticketsData = empSnap.getValue(TicketsModel::class.java)
+                        empList.add(ticketsData!!)
+                    }
+                    val mAdapter = EmpAdapter(empList)
+                    empRecyclerView.adapter = mAdapter
+
+                    empRecyclerView.visibility = View.VISIBLE
+                    tvLoadingData.visibility = View.GONE
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+        }
+    }
--- app/src/main/java/com/example/app_registo_de_tickets/Homepage.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/Homepage.kt
@@ -10,14 +10,21 @@ import androidx.fragment.app.Fragment
 class Homepage : AppCompatActivity() {
 
     private lateinit var btnInsertData: Button
+    private lateinit var btnFetchData: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
 
         btnInsertData= findViewById(R.id.btnInsertData)
+        btnFetchData = findViewById(R.id.btnFetchData)
 
         btnInsertData.setOnClickListener{
             val intent = Intent(this, InsertionActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFetchData.setOnClickListener {
+            val intent = Intent(this, FetchingActivity::class.java)
             startActivity(intent)
         }
     }
