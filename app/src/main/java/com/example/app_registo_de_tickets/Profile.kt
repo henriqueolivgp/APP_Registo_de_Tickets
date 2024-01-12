ae953dc3d72f2ab7c586db1511ab0581a4cda68c
SnowOwlS2 <mdaoliveira95@gmail.com>
2024-01-12 12:34:19 +0000

    Logout Button

--- app/src/main/java/com/example/app_registo_de_tickets/Profile.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/Profile.kt
@@ -4,13 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.app_registo_de_tickets.databinding.ActivityProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var user: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-    }
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        user = FirebaseAuth.getInstance()
+        binding.logoutButton.setOnClickListener {
+            user.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
     fun navegarParaHistory(view: View) {
         val intent = Intent(this, History::class.java)
 
