8197c4bcaa8d478ad6a589b616f134c391d353d0
henriqueolivgp <henrique.o@ipvc.pt>
2024-01-14 00:04:15 +0000

    create edit_profile.xml and EditProfile.kt

---
+++ app/src/main/java/com/example/app_registo_de_tickets/EditProfile.kt
@@ -0,0 +1,12 @@
+package com.example.app_registo_de_tickets
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_profile)
+    }
+}
