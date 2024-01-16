247fd54a26f70bf41f7d965492e0b6576bf42c3a
SnowOwlS2 <mdaoliveira95@gmail.com>
2024-01-16 17:14:49 +0000

    Profile linked with the user id

--- app/src/main/java/com/example/app_registo_de_tickets/EditProfile.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/EditProfile.kt
@@ -35,10 +35,8 @@ class EditProfile : AppCompatActivity() {
         databaseReference = FirebaseDatabase.getInstance().getReference("Profiles")
         auth = FirebaseAuth.getInstance()
 
-        // Retrieve current user's UID
         userId = auth.currentUser?.uid
 
-        // Load existing profile data if user has a profile
         if (userId != null) {
             checkExistingProfile(userId!!)
         }
@@ -49,17 +47,13 @@ class EditProfile : AppCompatActivity() {
             val empContacto = etEmpContacto.text.toString()
 
             if (empProfileName.isEmpty() || empLocalidade.isEmpty() || empContacto.isEmpty()) {
-                // Handle empty fields appropriately
                 return@setOnClickListener
             }
 
             if (userId != null) {
-                // Check if the user has an existing profile
                 if (isExistingProfile) {
-                    // Update the existing profile
                     updateProfile(userId!!, empProfileName, empLocalidade, empContacto)
                 } else {
-                    // Create a new profile with UID
                     createNewProfile(userId!!, empProfileName, empLocalidade, empContacto)
                 }
             } else {
@@ -71,32 +65,29 @@ class EditProfile : AppCompatActivity() {
     private var isExistingProfile: Boolean = false
 
     private fun checkExistingProfile(userId: String) {
-        // Check if the user has an existing profile
         databaseReference.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 isExistingProfile = snapshot.exists()
 
                 if (isExistingProfile) {
-                    // If the profile exists, load the existing data
+                    btnSaveData.text = "Atualizar Profile"
                     loadUserProfile(userId)
+                }else{
+                    btnSaveData.text = "Criar Profile"
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Handle the error
                 Toast.makeText(this@EditProfile, "Error checking existing profile: ${error.message}", Toast.LENGTH_SHORT).show()
             }
         })
     }
 
     private fun loadUserProfile(userId: String) {
-        // Retrieve the profile for the current user
         databaseReference.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                // Handle the retrieved profile data
                 val userProfile = snapshot.getValue(ProfileModel::class.java)
                 if (userProfile != null) {
-                    // Populate UI elements with existing profile data
                     etEmpProfileName.setText(userProfile.empProfileName)
                     etEmpLocalidade.setText(userProfile.empLocalidade)
                     etEmpContacto.setText(userProfile.empContacto)
@@ -104,42 +95,40 @@ class EditProfile : AppCompatActivity() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Handle the error
+
                 Toast.makeText(this@EditProfile, "Error loading profile: ${error.message}", Toast.LENGTH_SHORT).show()
             }
         })
     }
 
     private fun updateProfile(userId: String, empProfileName: String, empLocalidade: String, empContacto: String) {
-        // Update the existing profile with new data
+
         val profile = ProfileModel(userId, empProfileName, empLocalidade, empContacto)
 
         databaseReference.child(userId).setValue(profile)
             .addOnCompleteListener {
                 Toast.makeText(this, "Data updated successfully", Toast.LENGTH_LONG).show()
-                finish() // Finish the activity after updating the profile
+                finish()
             }.addOnFailureListener { err ->
                 Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
             }
     }
 
     private fun createNewProfile(userId: String, empProfileName: String, empLocalidade: String, empContacto: String) {
-        // Create a new profile with UID
         val profile = ProfileModel(userId, empProfileName, empLocalidade, empContacto)
 
-        // Save the new profile with the user's UID
         databaseReference.child(userId).setValue(profile)
             .addOnCompleteListener {
                 Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
-                finish() // Finish the activity after creating the new profile
+                finish()
             }.addOnFailureListener { err ->
                 Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
             }
     }
     private fun navigateToProfilePage() {
-        // Intent to start the ProfileActivity
+
         val intent = Intent(this, Profile::class.java)
         startActivity(intent)
-        finish() // Finish the current activity if you don't want to come back to it
+        finish()
     }
 }
