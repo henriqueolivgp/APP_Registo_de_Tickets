ad82ab5057e08b1a5f68a2866331b0be1ea485e6
SnowOwlS2 <mdaoliveira95@gmail.com>
2024-01-16 12:28:55 +0000

    Logout Button

--- app/src/main/java/com/example/app_registo_de_tickets/History.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/History.kt
@@ -1,11 +1,18 @@
 package com.example.app_registo_de_tickets
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class History : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+    }
+    fun navegarParaPerfil(view: View) {
+        val intent = Intent(this, Profile::class.java)
+
+        startActivity(intent)
     }
 }
