7d7b776e37b6ed8ae0bad72477ab5f24dbcee691
SnowOwlS2 <mdaoliveira95@gmail.com>
2024-01-18 16:51:48 +0000

    Profile linked with the user id

--- app/src/main/java/com/example/app_registo_de_tickets/History.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/History.kt
@@ -4,15 +4,89 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class History : AppCompatActivity() {
+    private lateinit var empRecyclerView: RecyclerView
+    private lateinit var tvLoadingData: TextView
+    private lateinit var empList: ArrayList<TicketsModel>
+    private lateinit var dbRef: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private var userId: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
-    }
-    fun navegarParaPerfil(view: View) {
-        val intent = Intent(this, Profile::class.java)
+        empRecyclerView = findViewById(R.id.rvEmp)
+        empRecyclerView.layoutManager = LinearLayoutManager(this)
+        empRecyclerView.setHasFixedSize(true)
+        tvLoadingData = findViewById(R.id.tvLoadingData)
 
-        startActivity(intent)
+        dbRef = FirebaseDatabase.getInstance().getReference("Tickets")
+        auth = FirebaseAuth.getInstance()
+
+        userId = auth.currentUser?.uid
+
+        empList = arrayListOf<TicketsModel>()
+        if (userId != null) {
+                getTicketsData()
+        } else {
+            Toast.makeText(this, "User not authenticated", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getTicketsData() {
+        empRecyclerView.visibility = View.GONE
+        tvLoadingData.visibility = View.VISIBLE
+
+        val ticketsReference = dbRef.orderByChild("empId").equalTo(userId)
+
+        ticketsReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                empList.clear()
+
+                for (empSnap in snapshot.children) {
+                    val ticketsData = empSnap.getValue(TicketsModel::class.java)
+                    if (ticketsData != null) {
+                        empList.add(ticketsData)
+                    }
+                }
+
+
+                val mAdapter = EmpAdapter(empList)
+                empRecyclerView.adapter = mAdapter
+
+                mAdapter.setOnItemClickListener(object : EmpAdapter.onItemClickListener {
+                    override fun onItemClick(position: Int) {
+                        val intent = Intent(this@History, HistoryDetailsDetails::class.java)
+
+                        intent.putExtra("empName", empList[position].empName)
+                        intent.putExtra("empLocalizacao", empList[position].empLocalizacao)
+                        intent.putExtra("empProblem", empList[position].empProblem)
+
+                        startActivity(intent)
+                    }
+                })
+
+                empRecyclerView.visibility = View.VISIBLE
+                tvLoadingData.visibility = View.GONE
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                empRecyclerView.visibility = View.GONE
+                tvLoadingData.visibility = View.VISIBLE
+                tvLoadingData.text = "Error loading data"
+            }
+        })
+
     }
 }
--- app/src/main/java/com/example/app_registo_de_tickets/InsertionActivity.kt
+++ app/src/main/java/com/example/app_registo_de_tickets/InsertionActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -14,6 +15,8 @@ class InsertionActivity : AppCompatActivity() {
     private lateinit var etEmpLocalizacao: EditText
     private lateinit var etEmpProblem: EditText
     private lateinit var btnSaveData: Button
+    private lateinit var auth: FirebaseAuth
+    private var userId: String? = null
 
     private lateinit var dbRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,42 +29,35 @@ class InsertionActivity : AppCompatActivity() {
         btnSaveData = findViewById(R.id.btnSave)
 
         dbRef = FirebaseDatabase.getInstance().getReference("Tickets")
+        auth = FirebaseAuth.getInstance()
+        userId = auth.currentUser?.uid
+
 
         btnSaveData.setOnClickListener {
-            saveTicketsData()
+            val empName = etEmpName.text.toString()
+            val empLocalizacao = etEmpLocalizacao.text.toString()
+            val empProblem = etEmpProblem.text.toString()
+
+            if (empLocalizacao.isEmpty() || empProblem.isEmpty() || empName.isEmpty()) {
+                return@setOnClickListener
+            }
+
+            if (userId != null) {
+                saveTicketsData(userId!!, empName, empLocalizacao,empProblem)
+            } else {
+                Toast.makeText(this, "User not authenticated", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    private fun saveTicketsData() {
+    private fun saveTicketsData(userId : String, empName : String, empLocalizacao: String, empProblem : String) {
 
-        //getting values
-        val empName = etEmpName.text.toString()
-        val empLocalizacao = etEmpLocalizacao.text.toString()
-        val empProblem = etEmpProblem.text.toString()
+        val employee = TicketsModel(userId, empName, empLocalizacao, empProblem)
 
-        if (empName.isEmpty()) {
-            etEmpName.error = "Introduza o nome da empresa"
-        }
-        if (empLocalizacao.isEmpty()) {
-            etEmpLocalizacao.error = "Introduza a localizacao da empresa"
-        }
-        if (empProblem.isEmpty()) {
-            etEmpProblem.error = "Introduza o problema"
-        }
-
-        val empId = dbRef.push().key!!
-
-        val employee = TicketsModel(empId, empName, empLocalizacao, empProblem)
-
-        dbRef.child(empId).setValue(employee)
+        dbRef.child(userId).setValue(employee)
             .addOnCompleteListener {
                 Toast.makeText(this, "Data inserted successfully", Toast.LENGTH_LONG).show()
-
-                etEmpName.text.clear()
-                etEmpLocalizacao.text.clear()
-                etEmpProblem.text.clear()
-
-
+                finish()
             }.addOnFailureListener { err ->
                 Toast.makeText(this, "Error ${err.message}", Toast.LENGTH_LONG).show()
             }
